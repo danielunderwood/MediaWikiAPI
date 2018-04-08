8d2b5c578d74e62ea59d97da537e6058bc2cdd1d
Daniel Underwood <daniel.underwood13@gmail.com>
2018-04-08 14:02:54 -0400

    Add case for setuptools

--- mediawikiapi/__init__.py
+++ mediawikiapi/__init__.py
@@ -1,9 +1,11 @@
 import sys
 
-if not (sys.argv[0] in ['pip', 'setup.py'] or (len(sys.argv) > 1 and sys.argv[1] in ['setup.py', 'egg_info'])):
+if not (sys.argv[0] in ['pip', 'setup.py']
+        or (len(sys.argv) > 1 and sys.argv[1] in ['setup.py', 'egg_info'])
+        or any([arg.startswith('import setuptools') for arg in sys.argv])):
     from .config import *
     from .mediawikiapi import *
     from .exceptions import *
     from .language import *
 
-__version__ = "1.1.3dev1"
+__version__ = "1.1.3"
