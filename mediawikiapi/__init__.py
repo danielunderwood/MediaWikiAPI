dbe8bf71fa7b845f9b880e274103b8d38ae920ec
Daniel Underwood <daniel.underwood13@gmail.com>
2018-04-08 14:09:50 -0400

    Fix case of bdist_wheel/sdist

--- mediawikiapi/__init__.py
+++ mediawikiapi/__init__.py
@@ -2,8 +2,7 @@ import sys
 
 print(sys.argv)
 if not (sys.argv[0] in ['pip', 'setup.py']
-        or (len(sys.argv) > 1 and sys.argv[1] in ['setup.py', 'egg_info'])
-        or any([arg.startswith('import setuptools') for arg in sys.argv])):
+        or (len(sys.argv) > 1 and sys.argv[1] in ['setup.py', 'egg_info', 'bdist_wheel', 'sdist'])):
     from .config import *
     from .mediawikiapi import *
     from .exceptions import *
